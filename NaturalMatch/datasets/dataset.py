467d2fe04ca63acdb5e014d750298a1a17bc6419
Sullens_Lee <49591058+lowKnow@users.noreply.github.com>
2024-09-30 20:31:41 +0800

    Add files via upload

---
+++ NaturalMatch/datasets/dataset.py
@@ -0,0 +1,76 @@
+import os
+
+from torchvision import transforms
+from torch.utils.data import Dataset
+from .data_utils import get_onehot
+from .augmentation.randaugment import RandAugment
+
+import torch
+
+import torchvision
+from PIL import Image
+import numpy as np
+import copy
+
+
+class BasicDataset(Dataset):
+
+    def __init__(self,
+                 alg,
+                 data,
+                 targets=None,
+                 num_classes=None,
+                 transform=None,
+                 is_ulb=False,
+                 strong_transform=None,
+                 onehot=False,
+                 *args, **kwargs):
+        super(BasicDataset, self).__init__()
+        self.alg = alg
+        self.data = data
+        self.targets = targets
+
+        self.num_classes = num_classes
+        self.is_ulb = is_ulb
+        self.onehot = onehot
+
+        self.transform = transform
+        if self.is_ulb:
+            if strong_transform is None:
+                self.strong_transform = copy.deepcopy(transform)
+                self.strong_transform.transforms.insert(0, RandAugment(3, 5))
+        else:
+            self.strong_transform = strong_transform
+
+    def __getitem__(self, idx):
+
+        # set idx-th target
+        if self.targets is None:
+            target = None
+        else:
+            target_ = self.targets[idx]
+            target = target_ if not self.onehot else get_onehot(self.num_classes, target_)
+
+        # set augmented images
+
+        img = self.data[idx]
+        if self.transform is None:
+            return transforms.ToTensor()(img), target
+        else:
+            if isinstance(img, np.ndarray):
+                img = Image.fromarray(img)
+            img_w = self.transform(img)
+            if not self.is_ulb:
+                return idx, img_w, target
+            else:
+                if self.alg == 'naturalmatch':
+                    return target, img_w, self.strong_transform(img)
+                else:
+                    raise Exception('Model name incorrect!!!')
+
+    def __len__(self):
+        return len(self.data)
+
+
+
+
